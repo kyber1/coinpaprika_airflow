80f3c7b6db4c5eeaa4160fc45478125dbe2bc5af
kyber1 <leonard.merva@gmail.com>
2024-10-20 11:37:31 +0200

    adding git ignore, removing parking lot folder

--- dev_parking_lot/scrape_top100.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://api-frontend.coinpaprika.com/ajax/coins/1?section=home-overview&all=false&expand=home-overview%2Cprice_stats&sort%5Bsort%5D=index&sort%5Bsortorder%5D=asc&tagID=&filters%5Bcoins%5D=true&filters%5Btokens%5D=true&currency=usd"
-
-response = requests.get(url)
-
-print(response)
